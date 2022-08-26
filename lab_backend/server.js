ec2f5337d8d0bffa606504e93871602bfda187ff
Miguel Castaneda <miguelcastaneda@Miguels-MBP.lan>
2022-08-26 09:23:44 -0700

    backend complete

--- lab_backend/server.js
+++ lab_backend/server.js
@@ -16,13 +16,26 @@ app.post('/games', (req, res) => {
     })
 })
 
+//index route
+app.get('/games', (req, res)=>{
+    Games.find({}, (err, foundGames)=>{
+        res.json(foundGames);
+    });
+});
 
+//delete route
+app.delete('/games/:id', (req, res)=>{
+    Games.findByIdAndRemove(req.params.id, (err, deletedGames)=>{
+        res.json(deletedGames);
+    });
+});
 
-
-
-
-
-
+//edit route
+app.put('/todos/:id', (req, res)=>{
+    Games.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, updatedGame)=>{
+        res.json(updatedGame);
+    });
+});
 
 app.listen(3000, () => {
     console.log('listening')
